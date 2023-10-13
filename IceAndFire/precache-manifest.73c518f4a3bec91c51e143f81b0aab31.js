2e7bd505120424d7a6f8e543b9b5e755c346b247
NikitaButorov <butorov.nikita@list.ru>
2023-10-13 13:11:08 +0300

    fitst deploy

---
+++ IceAndFire/precache-manifest.73c518f4a3bec91c51e143f81b0aab31.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f91a6a4b533477a8dc7",
+    "url": "./static/css/main.ebb4b722.chunk.css"
+  },
+  {
+    "revision": "5f91a6a4b533477a8dc7",
+    "url": "./static/js/main.5f91a6a4.chunk.js"
+  },
+  {
+    "revision": "4726e4842fa2406cf6f6",
+    "url": "./static/css/1.7e71805a.chunk.css"
+  },
+  {
+    "revision": "4726e4842fa2406cf6f6",
+    "url": "./static/js/1.4726e484.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "22389cf47ad284464c13506f2e22e8af",
+    "url": "./static/media/error.22389cf4.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "./static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "501e23f80ed1ca427ba532bf59bac8f7",
+    "url": "./index.html"
+  }
+];
