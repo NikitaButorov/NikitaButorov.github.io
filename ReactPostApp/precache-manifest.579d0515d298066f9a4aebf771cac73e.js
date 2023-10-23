b9b49ff5b473a0f31cc25ece96ee7974869f23b9
NikitaButorov <butorov.nikita@list.ru>
2023-10-23 09:54:14 +0300

    ReactPostApp deploy

---
+++ ReactPostApp/precache-manifest.579d0515d298066f9a4aebf771cac73e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c72449644a5e09f8317",
+    "url": "/ReactPostApp/static/css/main.d017fa59.chunk.css"
+  },
+  {
+    "revision": "7c72449644a5e09f8317",
+    "url": "/ReactPostApp/static/js/main.7c724496.chunk.js"
+  },
+  {
+    "revision": "23be1fc5341bfdce8ce5",
+    "url": "/ReactPostApp/static/css/1.58b34137.chunk.css"
+  },
+  {
+    "revision": "23be1fc5341bfdce8ce5",
+    "url": "/ReactPostApp/static/js/1.23be1fc5.chunk.js"
+  },
+  {
+    "revision": "e86d5af5118a8b47a968",
+    "url": "/ReactPostApp/static/js/runtime~main.e86d5af5.js"
+  },
+  {
+    "revision": "7dbfcd3ea094c9aae0b146624376fe9e",
+    "url": "/ReactPostApp/index.html"
+  }
+];
